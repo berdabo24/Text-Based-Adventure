b5060aa78b075f31bf02cc8bf46e4f7da2c9d0b3
scenery9 <samanthachan285@gmail.com>
2025-02-06 02:46:57 +0800

    Delete enemy.txt and character.txt when new game

--- filehandling.cpp
+++ filehandling.cpp
@@ -595,6 +595,8 @@ void MainMenu(){
 
     if (menuchoice == "New Game"){
         deleteFile("savefile");
+        deleteFIle("enemy.txt"); //prevent user from seeing previous game records as new game progress
+        deleteFile("character.txt"; //same as above
         autocreateFile("savefile", "outside");
         maingameloop(1);
     }
